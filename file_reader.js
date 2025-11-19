9fdafe9a4328469308ae57e53a96d8e39f195575
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-11-18 20:25:25 -0800

    WAD202_W1D1 completed exercise

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+  let sentence = data1.trim();
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    sentence += " " + data2.trim();
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      sentence += " is " + data3.trim() + " years old ";
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        const hobbiesArray = JSON.parse(data4);
+        sentence += `and his hobbies are ${hobbiesArray[0]} and ${hobbiesArray[1]}`;
+        console.log(sentence);
+      });
+    });
+  });
 })
